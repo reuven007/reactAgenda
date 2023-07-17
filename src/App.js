956f8bc4ea41c6216c6998a0649e5ef1c04e6c10
Reuven Martins <118221852+reuven007@users.noreply.github.com>
2023-07-17 18:54:14 +0000

    Update 3 files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from "react";
 import "./styles.css";
 
+
 import Navbar from "./components/Navbar/Navbar";
+import TaskList from "./components/TaskList/TaskList";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
-      <ul>
-        <li>Aprender Quark</li>
-        <li>Estudar 8h dia</li>
-      </ul>
+      <div>
+        <TaskList />
+      </div>
     </div>
   );
 }
